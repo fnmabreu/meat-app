a4f39a124ec46f72d03720ba6bbb8c0bd63b969d
fnmabreu <fnmabreu@gmail.com>
2018-05-09 19:05:52 +0100

    Estrutura do projeto criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mt-app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'mt-app';
+
+  content = 'Welcome do Meat App!';
 }
