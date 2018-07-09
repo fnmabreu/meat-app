942c31fcab5d659062b4a541fc4716049377fc10
Fábio Abreu <fnmabreu@gmail.com>
2018-07-09 23:51:40 +0100

    Realização de uma compra com autenticação

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,7 +1,7 @@
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 
 import { MEAT_API } from '../app.api';
@@ -9,6 +9,7 @@ import { Order } from './order.model';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
 import { MessageService } from '../message.service';
+import { LoginService } from '../security/login/login.service';
 
 @Injectable()
 export class OrderService {
@@ -18,6 +19,7 @@ export class OrderService {
   constructor(
     private cartService: ShoppingCartService,
     private http: HttpClient,
+    private loginService: LoginService,
     httpErrorHandler: HttpErrorHandler
   ) {
     this.handleError = httpErrorHandler.createHandleError('OrderService');
@@ -48,8 +50,14 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<Order> {
+    let headers = new HttpHeaders();
+
+    if (this.loginService.isLoggedIn()) {
+      headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`);
+    }
+
     return this.http
-      .post<Order>(this.ordersUrl, order)
+      .post<Order>(this.ordersUrl, order, { headers: headers})
       .pipe(catchError(this.handleError<Order>('checkOrder')));
   }
 }
--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,31 +1,41 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { LoginService } from "./login.service";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from './login.service';
+import { NotificationService } from '../../shared/messages/notification.service';
 
 @Component({
-  selector: "mt-app-login",
-  templateUrl: "./login.component.html"
+  selector: 'mt-app-login',
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
   emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private loginService: LoginService) {}
+  constructor(
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      email: this.fb.control("", [
+      email: this.fb.control('', [
         Validators.required,
         Validators.pattern(this.emailPattern)
       ]),
-      password: this.fb.control("", [Validators.required])
+      password: this.fb.control('', [Validators.required])
     });
   }
 
   login() {
     this.loginService
       .login(this.loginForm.value.email, this.loginForm.value.password)
-      .subscribe(user => console.log(user));
+      .subscribe(
+        user => this.notificationService.notify(`Bem-vindo, ${user.name}`),
+        (
+          response // HttpErrorResponse
+        ) => this.notificationService.notify(response.error.message)
+      );
   }
 }
